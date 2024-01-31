5ff6e2f1aa56adbcfe9a919cc00ac5d00d0a1f50
ramyareddy2457 <104816169+ramyareddy2457@users.noreply.github.com>
2024-01-31 12:08:37 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+1. Clone Project
+2. Run "npm install"
+3. Run "npm start"
+
+Open URL : http://localhost:3000/
---
+++ src/routes/route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const route = express.Router();
+
+const mainController = require("../controller/mainController");
+
+route.get("/",mainController.index)
+
+route.get("/projects",mainController.projects)
+
+
+route.all("/*", function (req, res) {
+    res.status(400).send({status: false,message: "The api you request is not available"})
+})
+module.exports = route;
